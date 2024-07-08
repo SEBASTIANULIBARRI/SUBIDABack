33bb8ed0ee43fd88c19d5bca3e8ffaa61b3eff95
SEBASTIANULIBARRI <145642465+SEBASTIANULIBARRI@users.noreply.github.com>
2024-07-08 18:08:35 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const dotenvconfig=require( 'dotenv/config');
+
+
+const moviesRoutes = require('./routes/moviesRoutes');
+const usersRoutes = require('./routes/usersRoutes');
+const vistaRoutes = require('./routes/vistaRoutes');
+const likeRoutes = require('./routes/likeMovieRoutes');
+const usersController = require('./controllers/usersController');
+
+//middleware
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', moviesRoutes);
+app.use('/api', usersRoutes);
+app.use('/api', vistaRoutes);
+app.use('/api', likeRoutes);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//files estaticos (hay que cambiarlo pero cumple funcionalidad)
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname,'index.html'));});
+app.get('/login', (req, res) => {res.sendFile(path.join(__dirname,'public','pages','login.html'));});
+app.get('/perfil', (req, res) => {res.sendFile(path.join(__dirname,'public','pages','perfil.html'));});
+app.get('/registrarse', (req, res) => {res.sendFile(path.join(__dirname,'public','pages','registrarse.html'));});
+app.get('/addMovie', (req, res) => {res.sendFile(path.join(__dirname,'public','pages','user.html'));});
+
+const PORT = process.env.ALWAYSDATA_HTTPD_PORT || 3000;
+const IP = process.env.ALWAYSDATA_HTTPD_IP || '127.0.0.1';
+
+app.listen(PORT, IP, () => {
+  console.log(`Server running at http://${IP}:${PORT}/`);
+});
---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB
+    
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error conectando a la base de datos:', err.stack);
+        return;
+    }
+    console.log('Conectado a la base de datos.');
+});
+
+module.exports = connection;
